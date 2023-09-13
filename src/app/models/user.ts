2b2c72e01175af6533eb9670a6e4314a4ee5df39
Primiano <primiano.trivisonno@gmail.com>
2023-09-13 06:00:44 +0200

    register done, ora login e authguard

---
+++ src/app/models/user.ts
@@ -0,0 +1,5 @@
+export interface Dipendente {
+  username: string;
+  email: string;
+  password: string;
+}
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpHeaders, HttpClient, HttpParams } from '@angular/common/http';
+
+
+interface ResponseData {
+  error_msg: string;
+  tag: string;
+  status: boolean;
+}
+
 
 @Component({
   selector: 'app-register',
@@ -7,6 +17,52 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  private readonly REGISTER_URL = 'http://localhost:8080/api/users/register';
+
   hide = true;
+
+  username: string;
+  email: string;
+  password: string;
+
+  constructor( private http: HttpClient, private router: Router) {
+    this.username = '';
+    this.email = '';
+    this.password = '';
+  }
+
+  register(){
+    const params = new HttpParams()
+    .set('username', this.username)
+    .set('email', this.email)
+    .set('password', this.password);
+
+  const httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+    params: params,
+  };
+
+    this.http.post<ResponseData>(this.REGISTER_URL, null, httpOptions).subscribe(
+      (response) => {
+        // Gestisci la risposta del server qui, se necessario.
+
+        window.alert(response.error_msg);
+
+
+
+        if(response.status == true){
+          window.alert("Ora esegui il login");
+          this.router.navigate(['/login']);
+        }
+        console.log('Risposta POST:', response.error_msg);
+      },
+      (error) => {
+        // Gestisci eventuali errori qui.
+        console.error('Errore POST:', error);
+      }
+    );
+  }
 
 }
